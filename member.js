c0d31bb64baff4f539ed670c827a8d5a5e6ef7cd
Alfonso <alfonsocespino10@gmail.com>
2024-09-17 01:23:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillMembers() {
+  return {
+    members: [
+      { name: 'John', skill: 'JavaScript' },
+      { name: 'Jane', skill: 'HTML' },
+      { name: 'Doe', skill: 'CSS' },
+    ],
+  };
+}
